44c229115372886fec1af7ed460bb1b9d4e999dd
Lenson-Lee <84387116+Lenson-Lee@users.noreply.github.com>
2022-11-29 20:38:10 +0900

    5-1

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,15 +5,70 @@ const socket = io();
 
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleNicknameSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("#name input");
+  const value = input.value;
+  socket.emit("nickname", value);
+  input.value = "";
+}
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("#msg input");
+  const value = input.value;
+  socket.emit("new_message", value, roomName, () => {
+    addMessage(`you : ${value}`);
+  });
+  input.value = "";
+}
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector("#msg");
+  const nameForm = room.querySelector("#name");
+  msgForm.addEventListener("submit", handleMessageSubmit);
+  nameForm.addEventListener("submit", handleNicknameSubmit);
+}
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value });
+  //socket.emit: 이벤트를 발생(emit)시키는 역할. 이벤트명을 임의로 지정(enter_room), 서버에서 발생시킨 이벤트명만 알면 처리가능.
+  //첫 번째 인자: 이벤트명, 두 번째 인자: 전송할 데이터(객체가능), 세 번째 인자: 서버에서 호출할 콜백 함수.
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (userNickName) => {
+  addMessage(`${userNickName} arrived`);
+});
+
+socket.on("bye", (userNickName) => {
+  addMessage(`${userNickName} left`);
+});
+
+socket.on("new_message", (msg) => {
+  addMessage(msg);
+});
 
 //========================================================
 // const socket = new WebSocket(`ws://${window.location.host}`);
